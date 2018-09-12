e3fc91f49912fe5d9b5e348ebfad62aaac76b83f
Tuan Nguyen <python.study.0708@gmail.com>
2018-09-13 00:30:13 +0700

    Django Tutorial - Version 1.10.8
    Django Tutorial - Version 1.10.8

---
+++ mysite-1.10.8/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-polls',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='A simple Django app to conduct Web-based polls. (For django 1.10)',
+    long_description=README,
+    url='https://github.com/garmoncheg/django-polls_1.10',
+    author='Yuri Garmash',
+    author_email='garmoncheg@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    keywords='sample setuptools development django polls app tutorial example',
+    install_requires=['django==1.10'],
+)
